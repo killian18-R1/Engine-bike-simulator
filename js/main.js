27a13382e92e4bd3ecaa14daff3be1c4d45d7591
killian18-R1 <killianazzoug18@gmail.com>
2026-07-28 01:15:59 +0200

    V0.2.0-a1 : Initialisation de la nouvelle architecture du projet Update main.js
    - Début de la refonte complète de MotoSim
    - Création de l'architecture modulaire
    - Ajout du dossier js
    - Ajout du dossier bikes
    - Création du GameState
    - Création du BikeLoader
    - Création des modules du simulateur
    - Ajout des premières fiches moto (Yamaha R1 et Sandbox)
    - Suppression de l'ancienne architecture monolithique basée sur script.js
    - Préparation du nouveau moteur physique V0.2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/*
+====================================
+MotoSim
+Main
+====================================
+*/
 
+async function startMotoSim(){
+
+    console.clear();
+
+    console.log("MotoSim V0.2.0");
+
+    await loadBike("yamaha_r1_2024.json");
+
+}
+
+startMotoSim();
