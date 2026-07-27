2e0a5c212be723e550eaa2e2e914ce9a9d9144fb
killian18-R1 <killianazzoug18@gmail.com>
2026-07-28 01:15:37 +0200

    V0.2.0-a1 : Initialisation de la nouvelle architecture du projet Update bikeLoader.js
    - Début de la refonte complète de MotoSim
    - Création de l'architecture modulaire
    - Ajout du dossier js
    - Ajout du dossier bikes
    - Création du GameState
    - Création du BikeLoader
    - Création des modules du simulateur
    - Ajout des premières fiches moto (Yamaha R1 et Sandbox)
    - Suppression de l'ancienne architecture monolithique basée sur script.js
    - Préparation du nouveau moteur physique V0.2

--- js/bikeLoader.js
+++ js/bikeLoader.js
@@ -1,1 +1,26 @@
+/*
+====================================
+MotoSim
+Bike Loader
+====================================
+*/
 
+async function loadBike(fileName){
+
+    try{
+
+        const response = await fetch("bikes/" + fileName);
+
+        gameState.bike = await response.json();
+
+        console.log("Moto chargée :", gameState.bike.name);
+
+    }
+
+    catch(error){
+
+        console.error("Erreur de chargement :", error);
+
+    }
+
+}
