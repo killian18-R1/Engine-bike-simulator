499b521d7aacb8bca8b3949bf8e4865966a6431f
killian18-R1 <killianazzoug18@gmail.com>
2026-07-28 01:20:21 +0200

    V0.2.0-a2 : Création du moteur physique Engine Update engine.js
    - Création du module Engine
    - Ajout gestion RPM
    - Ajout gestion gaz progressive
    - Ajout couple moteur
    - Ajout inertie moteur
    - Ajout ralenti
    - Ajout limiteur moteur
    - Mise en place boucle de simulation temps réel

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,134 @@
-console.log("Engine chargé");
+/*
+====================================
+MotoSim
+Engine Module
+Version 0.2.0-a2
+====================================
+*/
+
+
+function updateEngine(deltaTime){
+
+
+    if(!gameState.bike)
+        return;
+
+
+
+    const engine = gameState.engine;
+
+    const data = gameState.bike.engine;
+
+
+
+    // Gestion de la poignée
+
+    if(gameState.controls.gas){
+
+        engine.throttle += deltaTime * 3;
+
+    }
+
+    else{
+
+        engine.throttle -= deltaTime * 5;
+
+    }
+
+
+
+    // Limites gaz
+
+    if(engine.throttle > 1)
+        engine.throttle = 1;
+
+
+    if(engine.throttle < 0)
+        engine.throttle = 0;
+
+
+
+
+    /*
+    ===============================
+    Courbe moteur simplifiée
+    ===============================
+    */
+
+
+    let rpmRatio =
+    engine.rpm / data.redline;
+
+
+
+    let torqueFactor =
+    1 - Math.abs(rpmRatio - 0.55);
+
+
+
+    if(torqueFactor < 0)
+        torqueFactor = 0;
+
+
+
+
+    let torque = 
+    data.torque *
+    torqueFactor *
+    engine.throttle;
+
+
+
+    engine.torque = torque;
+
+
+
+
+    /*
+    ===============================
+    Evolution RPM
+    ===============================
+    */
+
+
+    let rpmAcceleration =
+    torque * 80;
+
+
+
+    if(engine.throttle > 0){
+
+        engine.rpm +=
+        rpmAcceleration * deltaTime;
+
+    }
+
+    else{
+
+        engine.rpm -=
+        400 * deltaTime;
+
+    }
+
+
+
+
+    // Ralenti
+
+    if(engine.rpm < data.idleRPM)
+
+        engine.rpm = data.idleRPM;
+
+
+
+    // Rupteur
+
+    if(engine.rpm > data.limiter){
+
+        engine.rpm = data.limiter;
+
+    }
+
+
+
+}
