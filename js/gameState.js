67d6724e25e30a95185993bacd10cdc45f3b7823
killian18-R1 <killianazzoug18@gmail.com>
2026-07-28 01:15:14 +0200

    V0.2.0-a1 : Initialisation de la nouvelle architecture du projet Update gameState.js
    - Début de la refonte complète de MotoSim
    - Création de l'architecture modulaire
    - Ajout du dossier js
    - Ajout du dossier bikes
    - Création du GameState
    - Création du BikeLoader
    - Création des modules du simulateur
    - Ajout des premières fiches moto (Yamaha R1 et Sandbox)
    - Suppression de l'ancienne architecture monolithique basée sur script.js
    - Préparation du nouveau moteur physique V0.2

--- js/gameState.js
+++ js/gameState.js
@@ -1,1 +1,61 @@
+/*
+====================================
+MotoSim
+GameState
+Version 0.2.0
+====================================
+*/
 
+const gameState = {
+
+    version: "0.2.0",
+
+    bike: null,
+
+    engine: {
+
+        rpm: 1200,
+
+        throttle: 0,
+
+        running: true
+
+    },
+
+    gearbox: {
+
+        gear: 0,
+
+        clutch: 1,
+
+        quickShifterUp: false,
+
+        quickShifterDown: false
+
+    },
+
+    physics: {
+
+        speed: 0,
+
+        acceleration: 0,
+
+        wheelRPM: 0
+
+    },
+
+    controls: {
+
+        gas: false,
+
+        brake: false,
+
+        shiftUp: false,
+
+        shiftDown: false
+
+    }
+
+};
+
+console.log("GameState chargé");
