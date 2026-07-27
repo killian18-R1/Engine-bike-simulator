717fd86b85a3d4d25c80dd5e207bc45dbbb22991
killian18-R1 <killianazzoug18@gmail.com>
2026-07-27 23:37:33 +0200

    V0.1.0-a2 : Simulation moteur basique script.js
    Ajout :
    - gestion gaz maintenu
    - montée et descente RPM
    - ralenti moteur
    - rupteur
    - relation RPM/vitesse

--- script.js
+++ script.js
@@ -4,92 +4,263 @@ let rpm = 1200;
 
 let speed = 0;
 
+let gas = false;
+
+let brake = false;
 
 
-// Passage supérieur
+// Limites moteur
 
-document.getElementById("plus").onclick = function(){
+const idleRPM = 1200;
 
-
-if(gear === "N"){
-
-gear = 1;
-
-}
-
-else if(gear < 6){
-
-gear++;
-
-}
+const maxRPM = 14000;
 
 
-document.getElementById("gear").innerHTML = gear;
 
+// Bouton gaz appuyé
+
+document.getElementById("gas").onmousedown = function(){
+
+    gas = true;
+
+};
+
+
+// Relâchement gaz
+
+document.getElementById("gas").onmouseup = function(){
+
+    gas = false;
+
+};
+
+
+// Compatibilité téléphone
+
+document.getElementById("gas").ontouchstart = function(e){
+
+    e.preventDefault();
+
+    gas = true;
+
+};
+
+
+document.getElementById("gas").ontouchend = function(){
+
+    gas = false;
 
 };
 
 
 
 
-// Passage inférieur
 
-document.getElementById("minus").onclick = function(){
+// Passage vitesse +
 
-
-if(gear === 1){
-
-gear="N";
-
-}
-
-else if(gear > 1){
-
-gear--;
-
-}
+document.getElementById("plus").onclick=function(){
 
 
-document.getElementById("gear").innerHTML = gear;
+    if(gear === "N"){
 
+        gear = 1;
+
+    }
+
+    else if(gear < 6){
+
+        gear++;
+
+    }
+
+
+    updateDisplay();
+
+};
+
+
+
+
+// Passage vitesse -
+
+document.getElementById("minus").onclick=function(){
+
+
+    if(gear === 1){
+
+        gear="N";
+
+    }
+
+    else if(gear > 1){
+
+        gear--;
+
+    }
+
+
+    updateDisplay();
 
 };
 
 
-
-
-// Gaz
-
-document.getElementById("gas").onclick=function(){
-
-
-rpm += 500;
-
-
-if(rpm > 14000){
-
-rpm = 14000;
-
-}
-
-
-document.getElementById("rpm").innerHTML=rpm;
-
-
-};
 
 
 
 
 // Frein
 
-document.getElementById("brake").onclick=function(){
+document.getElementById("brake").onmousedown=function(){
 
-
-speed = 0;
-
-
-document.getElementById("speed").innerHTML=speed;
-
+    brake=true;
 
 };
+
+
+document.getElementById("brake").onmouseup=function(){
+
+    brake=false;
+
+};
+
+
+
+document.getElementById("brake").ontouchstart=function(){
+
+    brake=true;
+
+};
+
+
+document.getElementById("brake").ontouchend=function(){
+
+    brake=false;
+
+};
+
+
+
+
+
+// Boucle moteur
+
+setInterval(function(){
+
+
+
+// Gestion gaz
+
+if(gas){
+
+
+    rpm += 250;
+
+
+}
+else{
+
+
+    rpm -= 150;
+
+
+}
+
+
+
+// Retour ralenti
+
+if(rpm < idleRPM){
+
+    rpm = idleRPM;
+
+}
+
+
+
+// Rupteur
+
+if(rpm > maxRPM){
+
+    rpm=maxRPM;
+
+}
+
+
+
+
+
+// Si une vitesse est engagée
+
+if(gear !== "N"){
+
+
+    let ratio = gear;
+
+
+    speed = rpm / (80 * ratio);
+
+
+
+}
+
+
+// Neutre
+
+else{
+
+
+    speed = 0;
+
+
+}
+
+
+
+
+
+// Frein
+
+if(brake){
+
+
+    speed -= 5;
+
+
+    if(speed < 0){
+
+        speed = 0;
+
+    }
+
+
+}
+
+
+
+
+
+updateDisplay();
+
+
+
+},50);
+
+
+
+
+
+
+
+function updateDisplay(){
+
+
+document.getElementById("rpm").innerHTML=Math.round(rpm);
+
+
+document.getElementById("speed").innerHTML=Math.round(speed);
+
+
+document.getElementById("gear").innerHTML=gear;
+
+
+}
