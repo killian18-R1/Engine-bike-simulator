0cd91bc55c2a6fb8eb065cc77d4674f77506ab45
killian18-R1 <killianazzoug18@gmail.com>
2026-07-28 01:21:39 +0200

    V0.2.0-a2 : Création du moteur physique Engine Update main.js
    - Création du module Engine
    - Ajout gestion RPM
    - Ajout gestion gaz progressive
    - Ajout couple moteur
    - Ajout inertie moteur
    - Ajout ralenti
    - Ajout limiteur moteur
    - Mise en place boucle de simulation temps réel

--- js/main.js
+++ js/main.js
@@ -1,18 +1,60 @@
 /*
 ====================================
 MotoSim
-Main
+Main Loop
+Version 0.2.0-a2
 ====================================
 */
 
+
 async function startMotoSim(){
+
 
     console.clear();
 
-    console.log("MotoSim V0.2.0");
+
+    console.log("MotoSim V0.2.0-a2");
+
 
     await loadBike("yamaha_r1_2024.json");
 
+
+    simulationLoop();
+
+
 }
+
+
+
+
+
+let lastTime = performance.now();
+
+
+
+function simulationLoop(){
+
+
+    let now = performance.now();
+
+
+    let deltaTime =
+    (now-lastTime)/1000;
+
+
+    lastTime = now;
+
+
+
+    updateEngine(deltaTime);
+
+
+
+    requestAnimationFrame(simulationLoop);
+
+
+}
+
+
 
 startMotoSim();
